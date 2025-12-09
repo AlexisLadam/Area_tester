d9a9b438a12b5ebf9e76913db3d21c7bf1e00fc0
AlexisLadam <alexis.ladam@epitech.eu>
2025-12-09 10:07:29 +0100

    a

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main() {
     printf("Hello, World!\n");
     printf("This project uses the C17 language standard version.\n");
+    printf("C17 introduces several improvements over previous standards.\n");
     return 0;
 }
